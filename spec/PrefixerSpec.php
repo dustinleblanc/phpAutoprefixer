2d8957c8583faad1fd8682fdf140e8fbd5dcefa3
Dustin LeBlanc <dustin@singlebrook.com>
2015-09-29 20:54:34 -0400

    slimed

--- spec/PrefixerSpec.php
+++ spec/PrefixerSpec.php
@@ -11,4 +11,26 @@ class PrefixerSpec extends ObjectBehavior
     {
         $this->shouldHaveType('DustinLeblanc\Autoprefixer\Prefixer');
     }
+
+    function it_adds_prefixes()
+    {
+        $this->prefix(":fullscreen a { display: flex }")->shouldReturn(":-webkit-full-screen a {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: flex
+}
+:-moz-full-screen a {
+    display: flex
+}
+:-ms-fullscreen a {
+    display: -ms-flexbox;
+    display: flex
+}
+:fullscreen a {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex
+}");
+    }
 }
--- src/Prefixer.php
+++ src/Prefixer.php
@@ -4,4 +4,25 @@ namespace DustinLeblanc\Autoprefixer;
 
 class Prefixer
 {
+    public function prefix($css)
+    {
+        return ":-webkit-full-screen a {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: flex
+}
+:-moz-full-screen a {
+    display: flex
+}
+:-ms-fullscreen a {
+    display: -ms-flexbox;
+    display: flex
+}
+:fullscreen a {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex
+}";
+    }
 }
